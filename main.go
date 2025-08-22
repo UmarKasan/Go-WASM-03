1c7efecec59236028f75dcd0c70d0e1468d22531
Umar Kasan <umarksn@gmail.com>
2025-08-22 22:24:53 +0800

    V1
    Simple Tic Tac Toe Game

--- main.go
+++ main.go
@@ -10,33 +10,38 @@ var (
 	playerState = "X"
 	player1     = "X"
 	player2     = "O"
-	statusEl    js.Value
-	boardEl     js.Value
+	status      js.Value
+	// board     js.Value
 )
 
 func main() {
 	document := js.Global().Get("document")
+	status := document.Call("querySelector", "#status")
+	status.Set("innerHTML", "Player "+playerState+"'s turn")
 	// Test script
-	// cell := document.Call("querySelector", ".ccell[data-index=\"0\"ell")
+	// cell := document.Call("querySelector", ".cell[data-index=\"0\"]")
 	// if !cell.IsUndefined() {
 	// 	cell.Set("innerHTML", "Hi")
 	// }
-	cell := document.Call("querySelector", ".cell[data-index=\"0\"]")
-	status := document.Call("querySelector", "#status")
-	status.Set("innerHTML", "Player "+playerState+"'s turn")
 
-	onClick := js.FuncOf(func(this js.Value, args []js.Value) any {
-		cell.Set("innerHTML", playerState)
-		// Change Player turn
-		if playerState == player1 {
-			playerState = player2
-		} else {
-			playerState = player1
-		}
-		return nil
-	})
-	cell.Call("addEventListener", "click", onClick)
+	// cell := document.Call("querySelector", ".cell[data-index=\"0\"]")
 
+	// Handle all 9 cells
+	for i := 0; i < 9; i++ {
+		cell := document.Call("querySelector", ".cell[data-index=\""+string(rune(i+'0'))+"\"]")
+		// Click Event
+		onClick := js.FuncOf(func(this js.Value, args []js.Value) any {
+			cell.Set("innerHTML", playerState)
+			// Change Player turn
+			if playerState == player1 {
+				playerState = player2
+			} else {
+				playerState = player1
+			}
+			return nil
+		})
+		cell.Call("addEventListener", "click", onClick)
+	}
 	// Keep running
 	select {}
 }
